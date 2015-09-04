1ac64686bde4ce213eb95f999c1f1277996d1ada
shaojieyue <4yxy1986@gmail.com>
2015-09-04 18:04:09 +0800

    dubbo-zipkin

--- dubbo-zipkin/src/main/java/com/self/ZipkinTraceFilter.java
+++ dubbo-zipkin/src/main/java/com/self/ZipkinTraceFilter.java
@@ -7,9 +7,17 @@ import com.alibaba.dubbo.rpc.*;
 import com.github.kristofa.brave.*;
 import com.github.kristofa.brave.zipkin.ZipkinSpanCollector;
 import com.github.kristofa.brave.zipkin.ZipkinSpanCollectorParams;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Collections;
 import java.util.Map;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 /**
  *
@@ -18,35 +26,129 @@ import java.util.Map;
  */
 @Activate(group = {Constants.CONSUMER, Constants.PROVIDER},order=-5000)
 public class ZipkinTraceFilter implements Filter {
+    public static final Logger logger = LoggerFactory.getLogger(ZipkinTraceFilter.class);
+    
     //采集trace
     public static final String SAMPLED = "0";
     //不采集trace
     public static final String NO_SAMPLED = "1";
-    private ZipkinSpanCollector collector;
-    private final ServerTracer serverTracer;
-    private final ClientTracer clientTracer;
+    public static final int PERIOD = 3;
 
+    private volatile boolean execSampled = false;
+    private ZipkinSpanCollector collector;
+    private  ServerTracer serverTracer;
+    private  ClientTracer clientTracer;
+    private ScheduledExecutorService service = Executors.newSingleThreadScheduledExecutor();
+    private Properties config = new Properties();
 
     public ZipkinTraceFilter() {
-        ZipkinSpanCollectorParams params = new ZipkinSpanCollectorParams();
-        params.setBatchSize(5);
-        params.setFailOnSetup(true);
-        params.setNrOfThreads(3);
-        params.setQueueSize(1000);
-        params.setSocketTimeout(2000);
-        collector = new ZipkinSpanCollector("10.10.20.105",9410,params);
-        serverTracer = Brave.getServerTracer(collector, Collections.EMPTY_LIST);
-        clientTracer = Brave.getClientTracer(collector, Collections.EMPTY_LIST);
+        //加载资源文件
+        final InputStream resourceAsStream = ZipkinTraceFilter.class.getClassLoader().getResourceAsStream("zipkin-clollector.properties");
+        if (resourceAsStream != null) {
+            try {
+                config.load(resourceAsStream);
+            } catch (IOException e) {
+                throw new IllegalArgumentException("load config [zipkin-clollector.properties] fail.");
+            }
+        }
+        initZipkin();
     }
 
-    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
-        final String serviceName = invoker.getUrl().getParameter(Constants.APPLICATION_KEY);
+    private void initZipkin() {
+        //定时检测收集器,防止收集器down重启
+        service.scheduleAtFixedRate(new Runnable() {
+            public void run() {
+                if (execSampled) {//已经启动,启动
+                    return;
+                }
+                /**
+                 * 单线程处理,所以没有必要使用同步锁
+                 */
+                final String zipkinCollectorHost = config.getProperty("zipkin.collector.host","localhost");
+                final int zipkinCollectorPort = Integer.valueOf(config.getProperty("zipkin.collector.port","1463"));
+                try {
+                    ZipkinSpanCollectorParams params = getZipkinSpanCollectorParams();
+                    if (collector != null) {
+                        try {
+                            collector.close();
+                        }catch (Throwable e){
+                        }
+                    }
+                    collector = new ZipkinSpanCollector(zipkinCollectorHost, zipkinCollectorPort,params);
+                    serverTracer = Brave.getServerTracer(collector, Collections.EMPTY_LIST);
+                    clientTracer = Brave.getClientTracer(collector, Collections.EMPTY_LIST);
+                    execSampled = true;
+                    if (logger.isInfoEnabled()) {
+                        logger.info("zipkin span collector start success");
+                    }
+                }catch (Throwable e){
+                    if (logger.isInfoEnabled()) {
+                        logger.info("zipkin span collector start fail. flume sink="+zipkinCollectorHost+":"+zipkinCollectorPort);
+                    }
+                    execSampled = false;
+                }
+                if (execSampled) {//启动成功则关闭定时器
+                    if (logger.isInfoEnabled()) {
+                        logger.info("zipkin started,shutdown the scheduled work.");
+                    }
+                    service.shutdown();
+                }
+            }
 
+            private ZipkinSpanCollectorParams getZipkinSpanCollectorParams() {
+                ZipkinSpanCollectorParams params = new ZipkinSpanCollectorParams();
+                //初始化默认配置
+                params.setBatchSize(20);//批量提交大小
+                params.setNrOfThreads(3);//提交span线程数
+                params.setQueueSize(1000);//存储span队列大小
+                params.setSocketTimeout(1000);//提交超时时间
+                if (config == null) {
+                    return params;
+                }
+
+                //批量提交大小
+                final String batchsize = config.getProperty("collector.batchsize");
+                if (batchsize != null) {
+                    params.setBatchSize(Integer.valueOf(batchsize));
+                }
+
+                //并行提交span的线程数
+                String nrOfThreads = config.getProperty("collector.nr.threads");
+                if (nrOfThreads != null) {
+                    params.setNrOfThreads(Integer.valueOf(nrOfThreads));
+                }
+
+                //缓存span的队列的大小
+                String queuesize = config.getProperty("collector.queuesize");
+                if (queuesize != null) {
+                    params.setQueueSize(Integer.valueOf(queuesize));
+                }
+
+                //socket time out
+                final String socketTimeout = config.getProperty("collector.socket.timeout");
+                if (socketTimeout != null) {
+                    params.setSocketTimeout(Integer.valueOf(socketTimeout));
+                }
+                return params;
+            }
+        },0, PERIOD, TimeUnit.SECONDS);//立即执行
+    }
+
+
+    public Result invoke(Invoker<?> invoker, Invocation invocation) throws RpcException {
+        if (!execSampled) {//不进行采样
+            return invoker.invoke(invocation);
+        }
+        final String serviceName = invoker.getUrl().getParameter(Constants.APPLICATION_KEY);
+        //serviceName 为空不进行采样
+        if (serviceName == null && "".equals(serviceName.trim())) {
+            return invoker.invoke(invocation);
+        }
         //注意：RpcContext是一个临时状态记录器，当接收到RPC请求，或发起RPC请求时，RpcContext的状态都会变化。
         // 比如：A调B，B再调C，则B机器上，在B调C之前，RpcContext记录的是A调B的信息，在B调C之后，RpcContext记录的是B调C的信息。
         // 所以要保持该filter的状态保证invoker.invoke(invocation);之后不会改变
         final RpcContext context = RpcContext.getContext();
-        final EndPointSubmitter endPointSubmitter = Brave.getEndPointSubmitter();
+        final EndpointSubmitter endPointSubmitter = Brave.getEndpointSubmitter();
         final int localPort = context.getLocalPort();
         final boolean providerSide = context.isProviderSide();
         final boolean consumerSide = context.isConsumerSide();
@@ -55,52 +157,67 @@ public class ZipkinTraceFilter implements Filter {
         final int remotePort = context.getRemotePort();
         //请求名称
         String requestName = invoker.getInterface().getName()+"."+invocation.getMethodName();
-
-        invokeBefore(invocation, serviceName, endPointSubmitter, localPort, providerSide, consumerSide, localHost, requestName);
-
+        try {
+            invokeBefore(invocation, serviceName, endPointSubmitter, localPort, providerSide, consumerSide, localHost, requestName);
+        }catch (Throwable e){}
         Result result = null;
-
         try {
             result = invoker.invoke(invocation);
-        }catch (RpcException e){
-            if (providerSide) {
-                serverTracer.submitBinaryAnnotation(remoteHost+remotePort+"//"+requestName,e.getCode());
-            }else if (consumerSide) {//调用端
-                clientTracer.submitBinaryAnnotation(remoteHost+remotePort+"//"+requestName,e.getCode());
-            }
         }finally {
-            invokeAfter(serviceName, endPointSubmitter, localPort, providerSide, consumerSide, localHost, remoteHost, remotePort, requestName, result);
+            try {
+                invokeAfter(serviceName, endPointSubmitter, localPort, providerSide, consumerSide, localHost, remoteHost, remotePort, requestName, result,invocation);
+            }catch (Throwable e){}
         }
         return result;
     }
 
-    private void invokeAfter(String serviceName, EndPointSubmitter endPointSubmitter, int localPort, boolean providerSide, boolean consumerSide, String localHost, String remoteHost, int remotePort, String requestName, Result result) {
+    private void invokeAfter(String serviceName, EndpointSubmitter endPointSubmitter, int localPort, boolean providerSide, boolean consumerSide, String localHost, String remoteHost, int remotePort, String requestName, Result result, Invocation invocation) {
         //由于ServerAndClientSpanState的单例,所以每次提交前都应该重新设置EndPoint,不然就会错乱
         endPointSubmitter.submit(localHost, localPort, serviceName);
-        if (providerSide) {
+        if (result == null || result.hasException()) {
+            final String key = remoteHost+ ":"+ remotePort + "//" + requestName;
+            StringBuilder params = new StringBuilder();
+            for (Object param : invocation.getArguments()) {
+                params.append(param).append(",");
+            }
+            String message = null;
+            if (result == null) {
+                message = "request time out";
+            }else {
+                message = result.getException().toString();
+            }
+            if (providerSide) {//server端
+                //设置remote信息 server端的remote为client,client的remote为server
+                serverTracer.submitBinaryAnnotation("client."+key, message);
+                serverTracer.submitBinaryAnnotation("client.params",params.toString());
+            }else if (consumerSide){
+                //设置remote信息 server端的remote为client,client的remote为server
+                clientTracer.submitBinaryAnnotation("server."+key, message);
+                clientTracer.submitBinaryAnnotation("server.params",params.toString());
+            }
+        }
+
+        if (providerSide) {//server端
             try {
                 serverTracer.setServerSend();
             } finally {
                 serverTracer.clearCurrentSpan();
             }
-        } else if (consumerSide) {//调用端
-            if (result!=null && result.hasException()) {
-                clientTracer.submitBinaryAnnotation(remoteHost+remotePort+"//"+requestName,result.getException().toString());
-            }
+        } else if (consumerSide) {//client端
             clientTracer.setClientReceived();
         }else {
         }
     }
 
-    private void invokeBefore(Invocation invocation, String serviceName, EndPointSubmitter endPointSubmitter, int localPort, boolean providerSide, boolean consumerSide, String localHost, String requestName) {
+    private void invokeBefore(Invocation invocation, String serviceName, EndpointSubmitter endPointSubmitter, int localPort, boolean providerSide, boolean consumerSide, String localHost, String requestName) {
         //由于ServerAndClientSpanState的单例,所以每次提交前都应该重新设置EndPoint,不然就会错乱
         endPointSubmitter.submit(localHost, localPort, serviceName);
-        if (providerSide) {
+        if (providerSide) {//server 端
             String traceId = invocation.getAttachment(ZipkinTraceConstants.TRACE_ID);
             String spanId = invocation.getAttachment(ZipkinTraceConstants.SPAN_ID);
             String parentId = invocation.getAttachment(ZipkinTraceConstants.PARENT_ID);
             String isSampled = invocation.getAttachment(ZipkinTraceConstants.IS_SAMPLED);
-            if (isSampled == null || SAMPLED.equals(isSampled)) {//无需搜集
+            if (isSampled == null || NO_SAMPLED.equals(isSampled)) {//无需搜集
                 serverTracer.setStateNoTracing();
             } else {
                 if (spanId == null) {
@@ -112,18 +229,19 @@ public class ZipkinTraceFilter implements Filter {
                 }
                 serverTracer.setServerReceived();
             }
-        }else if (consumerSide) {//调用端
+        }else if (consumerSide) {//client 端
+            //设置调用者的方法
             final SpanId span = clientTracer.startNewSpan(requestName);
             final Map<String, String> attachments = invocation.getAttachments();
             if (span == null) {//无需搜集
-                attachments.put(ZipkinTraceConstants.IS_SAMPLED, SAMPLED);
+                attachments.put(ZipkinTraceConstants.IS_SAMPLED, NO_SAMPLED);
             }else{//需要搜集数据
                 attachments.put(ZipkinTraceConstants.TRACE_ID, IdConversion.convertToString(span.getTraceId()));
                 attachments.put(ZipkinTraceConstants.SPAN_ID, IdConversion.convertToString(span.getSpanId()));
                 if (span.getParentSpanId() != null) {
                     attachments.put(ZipkinTraceConstants.PARENT_ID, IdConversion.convertToString(span.getParentSpanId()));
                 }
-                attachments.put(ZipkinTraceConstants.IS_SAMPLED, NO_SAMPLED);
+                attachments.put(ZipkinTraceConstants.IS_SAMPLED, SAMPLED);
             }
             try {
                 clientTracer.setClientSent();
@@ -133,67 +251,4 @@ public class ZipkinTraceFilter implements Filter {
             serverTracer.setStateNoTracing();
         }
     }
-
-
-//    public static void main(String[] args) throws InterruptedException {
-//        ZipkinTraceFilter filter = new ZipkinTraceFilter();
-//        Brave.getEndPointSubmitter().submit("192.168.5.52", (short) 9090, "com");
-//        for (int i=0;i<2;i++){
-//            final SpanId spanId = filter.clientTracer.startNewSpan("com.test");
-//            filter.clientTracer.setClientSent();
-//            Thread.sleep(1000);
-//
-//            if(spanId.getTraceId()>0){
-//                filter.serverTracer.setStateCurrentTrace(spanId.getTraceId(), spanId.getSpanId(), spanId.getParentSpanId(), "com.test.service");
-//            }else {
-//                filter.serverTracer.setStateUnknown("com.test.service");
-//            }
-//            filter.serverTracer.setServerReceived();
-//            Thread.sleep(2000);
-//            try {
-//                filter.serverTracer.setServerSend();
-//            } finally {
-//                filter.serverTracer.clearCurrentSpan();
-//            }
-//            filter.clientTracer.submitBinaryAnnotation("code",200);
-//            filter.clientTracer.setClientReceived();
-//            System.out.println("--->i"+i);
-//        }
-//
-//
-////        Random random = new Random();
-////        Span span = new Span();
-////        long rootSpanId=random.nextLong();
-////        span.setId(rootSpanId);
-////        span.setName("com");
-////        span.setTrace_id(rootSpanId);
-////        System.out.println(System.currentTimeMillis());
-////        final Endpoint endpoint = new Endpoint(IpUtil.ip2int("192.168.5.52"), (short) 9090, "com");
-////        final Annotation CLIENT_SEND = new Annotation(currentTimeMicroseconds(), zipkinCoreConstants.CLIENT_SEND);
-////        CLIENT_SEND.setHost(endpoint);
-////        span.addToAnnotations(CLIENT_SEND);
-////        Thread.sleep(1000);
-////        final Annotation SERVER_RECV = new Annotation(currentTimeMicroseconds(), zipkinCoreConstants.SERVER_RECV);
-////        SERVER_RECV.setHost(endpoint);
-////        span.addToAnnotations(SERVER_RECV);
-////        Thread.sleep(1000);
-////        final Annotation SERVER_SEND = new Annotation(currentTimeMicroseconds(), zipkinCoreConstants.SERVER_SEND);
-////        SERVER_SEND.setHost(endpoint);
-////        span.addToAnnotations(SERVER_SEND);
-////        Thread.sleep(2000);
-////        final Annotation CLIENT_RECV = new Annotation(currentTimeMicroseconds(), zipkinCoreConstants.CLIENT_RECV);
-////        CLIENT_SEND.setHost(endpoint);
-////        span.addToAnnotations(CLIENT_RECV);
-////        ZipkinSpanCollector collector = new ZipkinSpanCollector("10.10.20.105",9410);
-////        collector.collect(span);
-////        final Span span1 = span.deepCopy();
-////        span1.setParent_id(span.getId());
-////        span1.setId(random.nextInt());
-////        span1.setName("span1");
-////        collector.collect(span1);
-////        System.out.println("----------");
-//    }
-//    public static long currentTimeMicroseconds() {
-//        return System.currentTimeMillis()*1000 ;
-//    }
 }
